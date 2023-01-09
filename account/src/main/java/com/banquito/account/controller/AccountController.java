57f04ea86d72bde73887885c4af7f3d14072e1eb
Damoz1606 <panchitodmun@gmail.com>
2023-01-09 08:49:49 -0500

    Implement basics on controller and service

--- account/src/main/java/com/banquito/account/controller/AccountController.java
+++ account/src/main/java/com/banquito/account/controller/AccountController.java
@@ -1,8 +1,102 @@
 package com.banquito.account.controller;
 
+import java.math.BigDecimal;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class AccountController {
+import com.banquito.account.model.Account;
+import com.banquito.account.model.AccountSignature;
+import com.banquito.account.service.AccountService;
 
+@RestController
+@RequestMapping(value = "/api/account")
+public class AccountController {
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ResponseEntity<Object> createAccount(
+            @RequestBody Account account) {
+        return ResponseEntity.status(201).body("Object created");
+    }
+
+    @RequestMapping(value = "/{account-code}", method = RequestMethod.GET)
+    public ResponseEntity<Object> suspendAccount(
+            @PathVariable("account-code") String accountCode) {
+        return ResponseEntity.status(200).body("Suspend Account");
+    }
+
+    @RequestMapping(value = "/{account-code}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> updateAccountBalance(
+            @PathVariable("account-code") String accountCode,
+            @RequestBody BigDecimal balance) {
+        return ResponseEntity.status(200).body("Update Account Balance");
+    }
+
+    @RequestMapping(value = "/{account-code}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> inactiveAccount(
+            @PathVariable("account-code") String accountCode) {
+        return ResponseEntity.status(200).body("Change active account state");
+    }
+
+    @RequestMapping(value = "/{account-code}/statement", method = RequestMethod.GET)
+    public ResponseEntity<Object> getAccountStatement(
+            @PathVariable("account-code") String accountCode) {
+        return ResponseEntity.status(200).body("Account Statement");
+    }
+
+    @RequestMapping(value = "/{account-code}/signature", method = RequestMethod.GET)
+    public ResponseEntity<Object> getAssociatedSignature(
+            @PathVariable("account-code") String accountCode) {
+        return ResponseEntity.status(200).body("Get Associated Signature");
+    }
+
+    @RequestMapping(value = "/{account-code}/signature", method = RequestMethod.POST)
+    public ResponseEntity<Object> createAssociatedSignature(
+            @PathVariable("account-code") String accountCode,
+            @RequestBody AccountSignature signature) {
+        return ResponseEntity.status(200).body("Create Associated Signature");
+    }
+
+    @RequestMapping(value = "/{account-code}/signature/{signature-code}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> suspendAssociatedSignature(
+            @PathVariable("account-code") String accountCode,
+            @PathVariable("signature-code") String signatureCode) {
+        return ResponseEntity.status(200).body("Suspend Associated Signature");
+    }
+
+    @RequestMapping(value = "/{identification-type}/{identification}", method = RequestMethod.GET)
+    public ResponseEntity<Object> getConsolidatedPosition(
+            @PathVariable("identification-type") String identificationType,
+            @PathVariable("identification") String identification) {
+        return ResponseEntity.status(200).body("Consolidate Position");
+    }
+
+    @RequestMapping(value = "/{identification-type}/{identification}", method = RequestMethod.GET)
+    public ResponseEntity<Object> getClientAccounts(
+            @PathVariable("identification-type") String identificationType,
+            @PathVariable("identification") String identification) {
+        return ResponseEntity.status(200).body("Get Client Accounts");
+    }
+
+    @RequestMapping(value = "/product/service/{account-code}", method = RequestMethod.GET)
+    public ResponseEntity<Object> getAssociatedProducts(
+            @PathVariable("account-code") String accountCode) {
+        return ResponseEntity.status(200).body("Get Associated Products");
+    }
+
+    @RequestMapping(value = "/product/service/{account-code}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> inactiveProduct(
+            @PathVariable("account-code") String accountCode) {
+        return ResponseEntity.status(200).body("Change active product state");
+    }
 }
--- account/src/main/java/com/banquito/account/service/AccountService.java
+++ account/src/main/java/com/banquito/account/service/AccountService.java
@@ -1,11 +1,77 @@
 package com.banquito.account.service;
 
-import org.springframework.stereotype.Service;
+import java.math.BigDecimal;
+import java.util.List;
 
-/**
- * AccountService
- */
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.banquito.account.model.Account;
+import com.banquito.account.model.AccountSignature;
+import com.banquito.account.model.AccountStatementLog;
+import com.banquito.account.repository.AccountAssociatedServiceRepository;
+import com.banquito.account.repository.AccountClientRepository;
+import com.banquito.account.repository.AccountRepository;
+import com.banquito.account.repository.AccountSignatureRepository;
+import com.banquito.account.repository.AccountStatementLogRepository;
+
 @Service
 public class AccountService {
 
+    private final AccountRepository accountRepository;
+    private final AccountAssociatedServiceRepository accountAssociatedServiceRepository;
+    private final AccountClientRepository accountClientRepository;
+    private final AccountSignatureRepository accountSignatureRepository;
+    private final AccountStatementLogRepository accountStatementLogRepository;
+
+    public AccountService(AccountRepository accountRepository,
+            AccountAssociatedServiceRepository accountAssociatedServiceRepository,
+            AccountClientRepository accountClientRepository, AccountSignatureRepository accountSignatureRepository,
+            AccountStatementLogRepository accountStatementLogRepository) {
+        this.accountRepository = accountRepository;
+        this.accountAssociatedServiceRepository = accountAssociatedServiceRepository;
+        this.accountClientRepository = accountClientRepository;
+        this.accountSignatureRepository = accountSignatureRepository;
+        this.accountStatementLogRepository = accountStatementLogRepository;
+    }
+
+    @Transactional
+    public void createAccount(Account account) {
+    }
+
+    @Transactional
+    public void suspendAccount(String accountCode) {
+    }
+
+    @Transactional
+    public void updateAccountBalance(String accountCode, BigDecimal balance) {
+    }
+
+    @Transactional
+    public void inactiveAccount(String accountCode) {
+    }
+
+    public AccountStatementLog getAccountStatement(String accountCode) {
+        return null;
+    }
+
+    public AccountSignature getAssociatedSignature(String accountCode) {
+        return null;
+    }
+
+    @Transactional
+    public void createAssociatedSignature(String accountCode, AccountSignature signature) {
+    }
+
+    @Transactional
+    public void suspendAssociatedSignature(String accountCode, String signatureCode) {
+    }
+
+    public List<Account> getConsolidatedPosition(String identificationType, String identification) {
+        return null;
+    }
+
+    public List<Account> getClientAccounts(String identificationType, String identification) {
+        return null;
+    }
 }
